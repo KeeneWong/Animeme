b21d850c14694618360216451853b65f1cff65c6
KeeneWong <keenewonghofung@gmail.com>
2019-08-19 15:45:38 -0400

    detail finished

--- animeme/src/component/AnimeDetail/AnimeDetail.js
+++ animeme/src/component/AnimeDetail/AnimeDetail.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import "./AnimeDetail.css";
+import { Link } from "react-router-dom";
 
 class AnimeDetail extends Component {
   render() {
@@ -10,12 +11,24 @@ class AnimeDetail extends Component {
       <div
         class="animeDetail darken-pseudo darken-with-text"
         style={{
-          backgroundImage: `url(${thisanime[0].images.original})`,
-          backgroundColor: "rgba(0, 0, 0, 0.4)"
+          backgroundImage: `url(${thisanime[0].images.original})`
         }}
       >
-        <h1>detail</h1>
-        <h1>{thisanime[0].titles.en_jp}</h1>
+        <div class="leftDetail">
+          <div
+            class="detailimg"
+            style={{
+              backgroundImage: `url(${thisanime[0].images.original})`
+            }}
+          />
+        </div>
+        <div class="rightDetail">
+          <h1>{thisanime[0].titles.en_jp}</h1>
+          <h3>{thisanime[0].synopsis}</h3>
+          <Link to="/">
+            <h3 class="backbutton">Back</h3>
+          </Link>
+        </div>
       </div>
     );
   }
