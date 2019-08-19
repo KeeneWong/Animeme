e9b7b88d21c1152d0450e5c9e4037c8e4f8fa45f
KeeneWong <keenewonghofung@gmail.com>
2019-08-19 11:39:32 -0400

    init commit

---
+++ animeme/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <nav>
+        <h2>Animeme</h2>
+      </nav>
+      <main>
+        <h1>Main Part</h1>
+      </main>
+    </div>
+  );
+}
+
+export default App;
