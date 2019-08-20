0eefde34679bce5da294d4c46dd14b2b4301ee37
Kyle Clabough <kaclabough@gmail.com>
2019-08-20 16:16:07 -0400

    created user reducer

---
+++ animeme/src/reducers/user.js
@@ -0,0 +1,80 @@
+import {
+  CREATE_USER,
+  UPDATE_USER,
+  REMOVE_USER
+} from "../constants/userList.js";
+import axios from "axios";
+const searchUrl = "https://animeme-api.herokuapp.com/api/users";
+
+let users = [];
+
+var DEFAULT_STATE = {
+  users
+};
+
+async function getUsers() {
+  try {
+    let res = await axios({
+      url: searchUrl,
+      method: "get",
+      timeout: 1000,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    if (res.status === 200) {
+      console.log(res.status);
+    }
+    return res.data;
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+getUsers().then(user => {
+  let userList = res.map(user => {
+    let current = {
+      userName: user.userName,
+      password: user.password,
+      email: user.password,
+      favorites: user.favorites,
+      currentlyWatching: user.currentlyWatching
+    };
+    return current;
+  });
+  userList.forEach(user => {
+    users.push(user);
+  });
+});
+
+export default function userReducer(state = DEFAULT_STATE, action) {
+  switch (action.type) {
+    case CREATE_USER:
+      return {
+        ...state,
+        users: [...state.users, action.payload]
+      };
+    case UPDATE_USER:
+      return {
+        ...state,
+        users: state.users.map((user, index) => {
+          if (index !== action.payload.id) {
+            return user;
+          }
+          return {
+            ...user,
+            ...action.payload.updatedUser
+          };
+        })
+      };
+    case REMOVE_USER:
+      return {
+        ...state,
+        users: state.users.filter((user, id) => {
+          return id !== action.payload;
+        })
+      };
+    default:
+      return state;
+  }
+}
